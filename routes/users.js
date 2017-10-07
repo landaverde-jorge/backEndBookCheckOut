0d5aa12fbf6df3f1da2ef7975701fbbf68c63d52
Jorge Landaverde <jorgelandaverde@jorges-mbp.lan>
2017-10-06 18:07:46 -0700

    testing authentification

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const user = require('../models/user');
+const superagent = require('superagent');
+
+const AUTH_URL = 'https://slack.com/oauth/authorize';
+const CLIENT_ID = "232151668245.232194114629";
+const CLIENT_SECRET = "6fad7827bff771218d75a1eb2cc4751e";
+const SCOPES = 'identity.basic,identity.team,identity.avatar,identity.email';
+const ACCESS_URL = "https://slack.com/api/oauth.access";
 
 router.get('/', function(req, res, next) {
   user.query()
@@ -10,7 +17,17 @@ router.get('/', function(req, res, next) {
     .catch(next);
 });
 router.get('/authenticate', function(req, res, next) {
+  const code = req.query.code
+
+  superagent.get(ACCESS_URL)
+  .query({ client_id: CLIENT_ID, client_secret: CLIENT_SECRET}, code: code)
+  .end((err, res) => {
+    if (err) { return console.log(err); }
+    console.log(res, "================================",res.body)
+  });
+
   res.redirect("app://bookcheckout?token=helloworld")
+
 });
 router.post('/', function(req, res, next) {
   user.create(req.body)
