1391c860801ffe60b14859dccb3a1426a45f04ae
Jorge Landaverde <jorgelandaverde@Jorges-MBP.T-mobile.com>
2017-11-08 23:39:18 -0800

    aunthenticate testing

--- models/user.js
+++ models/user.js
@@ -7,4 +7,12 @@ exports.create = (data) => {
 }
 exports.find = (id) => {return User.findById(id)}
 exports.findBySlackId = (id) => {return User.find({key:id})}
-exports.authenticate = (id) => {return User.findById(id)}
+exports.authenticate = async (code) => {
+  const slackUser = (
+    await superagent
+    .get("https://slack.com/api/oauth.access")
+    .query({ client_id:"232151668245.232194114629", client_secret: "6fad7827bff771218d75a1eb2cc4751e", code: code})
+  ).body
+
+  console.log(slackUser)
+}
