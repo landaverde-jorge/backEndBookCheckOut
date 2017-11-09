07a5688bb4987c0096a80e99cb35b2a848a11350
Jorge Landaverde <jorgelandaverde@Jorges-MacBook-Pro.local>
2017-11-09 13:49:10 -0800

    aunthenticate testing

--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
 const User = require('../db/user');
 const superagent = require('superagent');
 
+const AUTH_URL = 'https://slack.com/oauth/authorize';
+const CLIENT_ID = "232151668245.232194114629";
+const CLIENT_SECRET = "6fad7827bff771218d75a1eb2cc4751e";
+const SCOPES = 'identity.basic,identity.team,identity.avatar,identity.email';
+const ACCESS_URL = "https://slack.com/api/oauth.access";
+
 exports.query = () => {return User.find({})}
 
 exports.create = (data) => {
@@ -13,8 +19,8 @@ exports.find = (id) => {return User.findById(id)}
 exports.authenticate = async (code) => {
   const slackUser = (
     await superagent
-    .get("https://slack.com/api/oauth.access")
-    .query({ client_id:"232151668245.232194114629", client_secret: "6fad7827bff771218d75a1eb2cc4751e", code: code})
+    .get(ACCESS_URL)
+    .query({ client_id:CLIENT_ID, client_secret: CLIENT_SECRET, code: code})
   ).body
   let user = await User.findOne({key:slackUser.user.id})
   if(!user){
@@ -26,7 +32,5 @@ exports.authenticate = async (code) => {
     })
     return user.save()
   }
-  console.log("+++++++++++++++++++++", slackUser.user.id)
-  console.log("+++++++++++++++++++++", user)
   return user
 }
